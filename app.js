8a0dcd2fc84f4ed85733d0e49953a625d299837b
tanshuangjun1 <907349004@qq.com>
2020-12-23 18:39:19 +0800

    配置热更新和路由从app.js分离；下一步实现路由和功能模块的分离

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const router = express.Router();
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/myDev/users');
 
 const app = express();
 
@@ -19,9 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 添加数据库表
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const usersRouter = require('./myDev/users');
+
+router.use('/users', usersRouter)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // mongoose
   res.render('index', { title: '双军的本地DEV环境数据库', baseName: 'myDev' });
 });
 
-module.exports = router;
+module.exports = router
